fa7c90d7d6bb7c4b441221486d0a3105969f55ba
Hassan Syam <enghassansyam@gmail.com>
2024-12-25 16:26:28 +0200

    init local env

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e626def42d634b42b3bb6c4a93454821d6e799904376e3df9b8d943dd4513169
+size 142
---
+++ keycloak.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9756ccc81f68515683e6cd91f87fa83768d52bfe0ab3fda2093ece992f9a43da
+size 363986
